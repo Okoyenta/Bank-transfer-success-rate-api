943c6cbb5e2392d23b38075d4bc2e83bdcfc11ab
ksworks <okoyentaeva@gmail.com>
2023-09-04 20:09:57 +0100

    db connect24

--- functions/controller/db.js
+++ functions/controller/db.js
@@ -1,20 +1,28 @@
 const mongoose = require('mongoose');
 
-const connectToDB = async () => {
-  try {
+let conn = null;
 
-       mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0qxab0k.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-       { useNewUrlParser: true, useUnifiedTopology: true })
-       
-       const db = mongoose.connection;
-          db.on('error', console.error.bind(console, 'connection error:'));
-          db.once('open', function() {
-          console.log('connected to db')
-          });
-  } catch (error) {
-    console.log(error);
-    console.log("mongoose Error");
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const db_name = process.env.DB_NAME
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.0qxab0k.mongodb.net/${db_name}?retryWrites=true&w=majority`
+
+const connect = async function() {
+  if (conn == null) {
+    conn = mongoose.connect(uri, {
+      useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000
+    }).then(() => mongoose);
+
+    // `await`ing connection after assigning to the `conn` variable
+    // to avoid multiple function calls creating new connections
+    await conn;
+
+    // Log a message when the database is connected
+    console.log('DB connected');
   }
-}
 
-module.exports = connectToDB;
+  return conn;
+};
+
+module.exports = connect
