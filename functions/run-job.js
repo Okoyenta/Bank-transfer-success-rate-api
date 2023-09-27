58d1d4511dbc6063004e533f045e886683450a85
ksworks <okoyentaeva@gmail.com>
2023-09-27 18:53:04 +0100

    secondg

--- functions/run-job.js
+++ functions/run-job.js
@@ -6,7 +6,7 @@ exports.handler = async function (event, context) {
   
     // Add your job logic here
     // For example, if you want to execute the initiatePay() function:
-    axios.get('https://transfer-rate.netlify.app/app/transfers')
+    axios.get('https://transfer-rate.netlify.app/app/transfer')
   .then(function (response) {
     // Handle success (status code 2xx)
     console.log('Data:', response.data);
