57ddb0df9e97d9ef8cd3c643e90b404b432ff8df
ksworks <okoyentaeva@gmail.com>
2023-09-27 16:58:55 +0100

    secondg

--- functions/run-job.js
+++ functions/run-job.js
@@ -1,19 +1,4 @@
-const serverless = require('serverless-http');
-const express = require('express')
-const cron = require('node-cron')
-const getBank = require('./controller/bankController')
-const { transferToBank, webhook, initiatePay, del } = require("./controller/transferController")
-const connect = require('./controller/db')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-const app = express()
-const router = express.Router()
-
-const PORT = process.env.PORT
-const user = process.env.DB_USER
-const password = process.env.DB_PASSWORD
-const db_name = process.env.DB_NAME
+const axios = require("axios");
 
 
 exports.handler = async function (event, context) {
@@ -21,8 +6,15 @@ exports.handler = async function (event, context) {
   
     // Add your job logic here
     // For example, if you want to execute the initiatePay() function:
-    connect()
-    initiatePay();
+    axios.get('https://transfer-rate.netlify.app/app/transfers')
+  .then(function (response) {
+    // Handle success (status code 2xx)
+    console.log('Data:', response.data);
+  })
+  .catch(function (error) {
+    // Handle error (status code 4xx or 5xx)
+    console.error('Error:', error);
+  });
   
     return {
       statusCode: 200,
